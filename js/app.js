e624e836d7d60cb1eef6996b3659417550117a5c
Christopher33 <christopher.couty@lapiscine.pro>
2020-09-30 13:24:05 +0200

    last stepp

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 
-function prevSlide() {
-    let element = document.getElementById("carouselle");
+function prevSlide1() {
+    let element = document.getElementById("carousel");
     element.style.left = "-100%";
 
+}
+
+function prevSlide2() {
+    let element = document.getElementById("carousel");
+    element.style.left = "-200%";
+
+}
+
+function prevSlide3() {
+    let element = document.getElementById("carousel");
+    element.style.left = "0%";
+
+}
+
+function openNav() {
+    let element = document.getElementById("nav_mobile_open");
+    element.style.display = "flex";
+    
+    let element2 = document.getElementById("nav_mobile");
+    element2.style.display = "none";
+}
+
+function blackIsBlack(){
+    let element = document.getElementById("nav_mobile_open");
+    element.style.display = "none";
+
+    let element2 = document.getElementById("nav_mobile");
+    element2.style.display = "flex";
 }
